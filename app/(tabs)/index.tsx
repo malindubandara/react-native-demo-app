fef5f7606255b8c31017c20ca97156484ae9d155
Malindu Bandara <malindubandara48@gmail.com>
2026-09-17 02:49:41 +0530

    feat: integrate custom fonts and update layout components for improved styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,9 +8,7 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extra-bold">Home</Text>
     </SafeAreaView>
   );
 }
